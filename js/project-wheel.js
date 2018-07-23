a80487c79733034eb5d77d454f8719185526deeb
webwaffle <sam525watchman@gmail.com>
2018-07-23 10:11:18 -0400

    Finished Homepage

--- js/project-wheel.js
+++ js/project-wheel.js
@@ -1,21 +1,35 @@
 var projects = [
-  {
-    name: "Waffle",
-    desc: "100% Organic Social Media"
+    {
+        name: "Waffle",
+        desc: "100% Organic Social Media"
   },
-  {
-    name: "Syrup",
-    desc: "A simple Node.js chat application"
+    {
+        name: "Syrup",
+        desc: "A simple Node.js chat application"
   }
 ]
-var currentIndex = 0;
-function setWheel (index) {
-  var p = projects[index];
-  document.querySelector('#project-name').innerHTML = p.name;
-  document.querySelector('#project-desc').innerHTML = p.desc;
+/*var currentIndex = 0;
+
+function setWheel(index) {
+    var p = projects[index];
+    if(!p) {
+        p = projects[0];
+        currentIndex = 0;
+    }
+    document.querySelector('#project-name').innerHTML = p.name;
+    document.querySelector('#project-desc').innerHTML = p.desc;
+    console.log('set');
 }
-setInterval(2000, function() {
-  setWheel(currentIndex + 1);
-  currentIndex++;
-});
 setWheel(0);
+setInterval(function () {
+    /*if (currentIndex - 1 > projects.length) {
+        console.log('aaaaa');
+        setWheel(0);
+        currentIndex = 0;
+    } else {
+    setWheel(currentIndex + 1);
+    //}
+    currentIndex++;
+    console.log('rotated');
+}, 2000);*/
+var projectSlide = w3.slideshow('.project', 1500);
